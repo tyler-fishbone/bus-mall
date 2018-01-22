7162a011ffe581ec66415996a3ecf583bce3b6fb
Tyler Fishbone <tyler.fishbone@gmail.com>
2018-01-22 15:10:11 -0800

    pseudocode for app.js created

---
+++ js/app.js
@@ -0,0 +1,71 @@
+'use strict';
+
+// global variables
+//    array to hold all products
+//    var setsOfThreeShown
+
+// constructor for products
+//    properties: filepath, name, times displayed, times clicked
+
+// function for choosing a random picture
+//    ++ times displayed for each
+
+// function for rendering 3 pictures
+//    ++ overall counter
+//    ++ times clicked
+//      if timesclicked == 25 show results
+
+// function to display results after 25 guesses
+
+// instantiate instances of the goat function
+
+// event listener
+
+// call function for initially choosing a random picture
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // array to store all goat instances
+  // make a constructor for Goat objects
+  // create instances of Goats
+  // access the image element from the DOM
+  // event listener on the image
+  // callback function for event listener to randomly display a goat image
+    // random number generator to return a number between 0 and the length of the array (Goat.allGoats)
+    // use the random number to display a goat at that random index
+  // invoke the callback on page load to show a random baby goat
